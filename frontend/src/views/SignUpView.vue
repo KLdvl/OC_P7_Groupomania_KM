f4a9794af7ac5d0a4c349dcce50f58a2c0c092c6
Kevin Mas <maskevin27@gmail.com>
2022-06-22 23:17:07 +0200

    Creating sign up page

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="signup">
     <h1>Please enter E-mail and password to sign up</h1>
-    <form @submit="onSubmit" class="add-form">
+    <v-form @submit="onSubmit" class="add-form">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field label="E-mail" variant="solo" placeholder="Enter your e-mail address">
+            </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="6" md="3">
+          <v-text-field label="Password" variant="solo" placeholder="Enter your password">
+          </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="6" md="3">
+            <v-btn
+                    append
+            >Sign Up
+              </v-btn>
+          </v-col>
+        </v-row>
+
+      </v-container>
       <div class="form-control">
         <label>E-mail Address</label>
         <input
@@ -21,13 +44,13 @@
         />
       </div>
       <input type="submit" value="Sign Up" class="btn btn-block" />
-    </form>
+    </v-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SignUp.vue",
+  name: "SignUp.vue"
 };
 </script>
 
