55d9d8c27bde822d4999d950599e2743641ee285
Kevin Mas <maskevin27@gmail.com>
2022-06-27 15:01:43 +0200

    Post display logic

--- frontend/src/components/PostCard.vue
+++ frontend/src/components/PostCard.vue
@@ -1,64 +1,52 @@
 <template>
-    <v-card class="mx-auto my-12" max-width="300">
+    <v-card v-for="post in posts" :key="post._id" class="mx-auto my-12" max-width="300">
         <v-img height="250">
         </v-img>
         <v-card-header>
             <v-card-header-text>
-                <v-card-title>test</v-card-title>
+                <v-card-title>{{post.title}}</v-card-title>
                 <v-card-subtitle>
-                    <span class="mr-1">Date de création du post</span>
+                    <span class="mr-1">{{post.date}}</span>
                 </v-card-subtitle>
             </v-card-header-text>
         </v-card-header>
         <v-card-text>
         </v-card-text>
     </v-card>
+    <div>{{posts}}</div>
 </template>
 
 <script>
-    const serverUrl = "http://localhost:8080/api/post/";
-    const parsedStorage = JSON.parse(localStorage.user)
-    const token = parsedStorage.token
-
     export default {
         name: "PostCard",
+        props: {},
         data() {
             return {
-                getResult: null
+                posts: []
             }
         },
-        methods: {
-            formatResponse(res) {
-                return JSON.stringify(res, null, 2);
-            },
-            async getAllData() {
-                try {
-                    const res = await fetch(serverUrl, {
-        method: "GET",
-        mode: "cors",
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
-                    const data = await res.json();
-                    const result = {
-                        status: res.status + "-" + res.statusText,
-                        headers: {
-                            "Content-Type": res.headers.get("Content-Type"),
-                            "Content-Length": res.headers.get("Content-Length"),
-                        },
-                        length: res.headers.get("Content-Length"),
-                        data: data,
-                    };
-                    this.getResult = this.formatResponse(result);
-                } catch (err) {
-                    this.getResult = err.message;
-                }
-            },
-        },
         mounted() {
-            this.getAllData()
+                    const serverUrl = "http://localhost:8080/api/post/";
+                    const parsedStorage = JSON.parse(localStorage.user)
+                    const token = parsedStorage.token
+                    const requestOptions = {
+                        method: 'GET',
+                        mode: 'cors',
+                        headers: {
+                            Authorization: `token ${token}`
+                        }
+                    }
+                    fetch(serverUrl, requestOptions)
+            .then(res => {
+                if(!res.ok) {
+                    const error = new Error(res.statusText);
+                    error.json = res.json();
+                    throw error;
+                }
+                return res.json();
+            })
+            .then(data => this.posts = data)
+            .catch(err => console.log(err.message))
         }
     }
 </script>
-
--- frontend/src/views/AllPosts.vue
+++ frontend/src/views/AllPosts.vue
@@ -1,11 +1,49 @@
 <template>
     <div>
         <h1 class="text-center" >Posts les plus récents</h1>
-<PostCard />
+        <PostCard />
+        <div></div>
     </div>
 </template>
 
-<script setup>
+<script>
+
     import PostCard from "../components/PostCard";
+    // const serverUrl = "http://localhost:8080/api/post/";
+    // const parsedStorage = JSON.parse(localStorage.user)
+    // const token = parsedStorage.token
+
+    export default {
+        name: "AllPosts",
+        components: {PostCard},
+        // data() {
+        //     return {
+        //         getResult: null
+        //     }
+        // },
+        // methods: {
+        //     formatResponse(res) {
+        //         return JSON.stringify(res, null, 2);
+        //     },
+        //     async getAllData() {
+        //         try {
+        //             const res = await fetch(serverUrl, {
+        //                 method: "GET",
+        //                 mode: "cors",
+        //                 headers: {
+        //                     Authorization: `token ${token}`
+        //                 }
+        //             });
+        //             const data = await res.json();
+        //             this.getResult = this.formatResponse(data);
+        //         } catch (err) {
+        //             this.getResult = err.message;
+        //         }
+        //     },
+        // },
+        // beforeMount() {
+        //     this.getAllData()
+        // }
+    }
 </script>
 
